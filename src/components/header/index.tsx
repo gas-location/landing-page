da3cd603c1d0381b3de4f05921a4c6fa78e03f28
git-marcopitra <marcomorete73@hotmail.com>
2021-08-19 13:31:04 +0100

    fix: iPhone nav bar height

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -17,35 +17,37 @@ import { HeaderProps } from './header.types';
 const Header: FC<HeaderProps> = ({ navItems, navLogo }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <HeaderWrapper>
-      <HeaderContent>
-        <LogoWrapper to="/">
-          <Image src={navLogo.image} alt={navLogo.name} loading="lazy" />
-        </LogoWrapper>
-        <Nav>
+    <>
+      <HeaderWrapper>
+        <HeaderContent>
+          <LogoWrapper to="/">
+            <Image src={navLogo.image} alt={navLogo.name} loading="lazy" />
+          </LogoWrapper>
+          <Nav>
+            {navItems.map(({ name, id }) => (
+              <HeaderLink key={id} to={`/#${id}`}>
+                {name}
+              </HeaderLink>
+            ))}
+          </Nav>
+          <Menu>
+            <BarsSVG
+              width="1.5rem"
+              onClick={() => setIsMenuOpen((state) => !state)}
+            />
+          </Menu>
+        </HeaderContent>
+      </HeaderWrapper>
+      {isMenuOpen && (
+        <MenuList>
           {navItems.map(({ name, id }) => (
             <HeaderLink key={id} to={`/#${id}`}>
-              {name}
+              <MenuItem>{name}</MenuItem>
             </HeaderLink>
           ))}
-        </Nav>
-        <Menu>
-          <BarsSVG
-            width="1.5rem"
-            onClick={() => setIsMenuOpen((state) => !state)}
-          />
-          {isMenuOpen && (
-            <MenuList>
-              {navItems.map(({ name, id }) => (
-                <HeaderLink key={id} to={`/#${id}`}>
-                  <MenuItem>{name}</MenuItem>
-                </HeaderLink>
-              ))}
-            </MenuList>
-          )}
-        </Menu>
-      </HeaderContent>
-    </HeaderWrapper>
+        </MenuList>
+      )}
+    </>
   );
 };
 
