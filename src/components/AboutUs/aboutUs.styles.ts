ff9d25388d9ce9cc025c256e517f025cd7c2f529
Milton Bernardo <miltonbernardo11@gmail.com>
2021-08-19 20:26:59 +0100

    completion of structure

--- src/components/AboutUs/aboutUs.styles.ts
+++ src/components/AboutUs/aboutUs.styles.ts
@@ -1,10 +1,61 @@
 import styled from '@emotion/styled';
 
-
-export const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr); 
-  padding: 2vh 5vh;
+export const AboutUsWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  @media (min-width: 600px) {
+    flex-direction: row;
+  }
 `;
 
+export const LogoImage = styled.img`
+  width: 60%;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+export const Subtitle = styled.h3`
+  font-weight: 600;
+  margin-bottom: 1%;
+  font-size: 1.5rem;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 0.5%;
+  font-weight: 400;
+  font-size: 2rem;
+`;
+
+export const Description = styled.p`
+  font-size: 1.15rem;
+  font-weight: 300;
+`;
+
+export const Values = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  margin-top: 15%;
+
+  & > div {
+    display: flex;
+    flex-flow: column nowrap;
+    width: 40%;
+    margin: 2.5% 5%;
+  }
+
+  & > div > p,
+  & > div > h4 {
+    text-align: center;
+    margin: 2% 0;
+  }
+  @media (min-width: 600px) {
+    flex-wrap: nowrap;
+    & > div {
+      margin: 2.5% 2.5%;
+    }
+  }
+`;
--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,17 +1,35 @@
 import React, { FC } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import {Container} from './aboutUs.styles';
+import {AboutUsWrapper, LogoImage, ContentWrapper,Subtitle,Title,Description,Values} from './aboutUs.styles';
 
 const AboutUs: FC = () => (
-    <Container> 
-      <StaticImage
-      src="../images/logo.png"
-    
-      quality={95}
-      alt="A Gatsby astronaut"
-      
-    />
-    </Container>
+    <AboutUsWrapper> 
+      <LogoImage src="./../images/logo.png" alt="background" loading="lazy"/>
+      <ContentWrapper>
+          <div>
+            <Subtitle>Quem Somos?</Subtitle>
+            <Title>Gas Location</Title>
+            <Description>Somos uma startup angolana que visa criar uma plataforma que permita você saber em tempo real, a disponibilidade de diversos serviços conectados ao seu automóvel. Desde empresas distribuidoras de combustível à revendedores de gás, estações de serviço e muito mais!</Description>
+          </div>
+          <Values>
+            <div>
+              <img src="" alt=""/>
+                <h4><strong>Missão</strong></h4>
+                <p>Disponibilizar informação em tempo real</p>
+            </div>
+            <div>
+              <img src="" alt=""/>
+              <h4><strong>visão</strong></h4>
+              <p>Alcançar o maior número de serviços de automóveis.</p>
+              </div>
+              <div>
+                <img src="" alt=""/>
+                  <h4><strong>Valores</strong></h4>
+                  <p>Comprometimento, transparência e Vontade.</p>
+              </div>
+          </Values>
+      </ContentWrapper>
+    </AboutUsWrapper>
   );
   
   export default AboutUs;
